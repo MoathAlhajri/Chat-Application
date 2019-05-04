e90459fbf7da6deda5407d1c8dd27b8ad61a164c
Moath <mooath97@gmail.com>
2019-05-04 16:40:07 +0300

    Add GUI for the server to display the server status

--- src/Server.java
+++ src/Server.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Server {
 
     private ServerSocket serverSocket;
+    private ServerFrame serverFrame;
     public ArrayList<ServerConnection> serverConnections = new ArrayList();
     public ArrayList username = new ArrayList();
 
@@ -15,18 +16,26 @@ public class Server {
 
     public Server() {
 
+        serverFrame = new ServerFrame();
+
         try {
 
-            System.out.println("Server init...");
+            serverFrame.statusArea.append("Server init... \n");
+
             serverSocket = new ServerSocket(3333);
             username.add("Online Users : ");
+            serverFrame.statusArea.append("Server start \n");
 
             while(true){
+
+                serverFrame.statusArea.append("Server waiting... \n");
 
                 Socket socket = serverSocket.accept();
                 ServerConnection serverConnection = new ServerConnection(socket, this);
                 serverConnection.start();
                 serverConnections.add(serverConnection);
+
+                serverFrame.statusArea.append("User connected \n");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,6 +48,8 @@ public class Server {
             if(serverConnections.get(i).toString().equals(serverConnection.toString())) {
                 serverConnections.remove(i);
                 username.remove(i+1);
+                serverFrame.statusArea.append("User logoff \n");
+                serverFrame.statusArea.append("Server waiting... \n");
                 break;
             }
     }
